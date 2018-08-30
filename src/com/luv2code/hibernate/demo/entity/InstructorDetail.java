2d0e80a7fa4bb1bad1ccbe8eb10716d53cf3ae75
GalynaSimonova <simonov.galyna@gmail.com>
2018-08-30 10:28:28 +0300

    Build Main App

---
+++ src/com/luv2code/hibernate/demo/entity/InstructorDetail.java
@@ -0,0 +1,98 @@
+package com.luv2code.hibernate.demo.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="instructor_detail")
+public class InstructorDetail {
+
+	//annotate the class as an entity and map to db table
+	
+	//define the fields
+	
+	//annotate the fields with db column names
+	
+	//create constructors
+	
+	//generate getter/setter methods
+	
+	//generate toString() method
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="youtube_channel")
+	private String youtubeChannel;
+	
+	@Column(name="hobby")
+	private String hobby;
+	
+	public InstructorDetail() {
+		
+	}
+
+	public InstructorDetail(String youtubeChannel, String hobby) {
+		this.youtubeChannel = youtubeChannel;
+		this.hobby = hobby;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the youtubeChannel
+	 */
+	public String getYoutubeChannel() {
+		return youtubeChannel;
+	}
+
+	/**
+	 * @param youtubeChannel the youtubeChannel to set
+	 */
+	public void setYoutubeChannel(String youtubeChannel) {
+		this.youtubeChannel = youtubeChannel;
+	}
+
+	/**
+	 * @return the hobby
+	 */
+	public String getHobby() {
+		return hobby;
+	}
+
+	/**
+	 * @param hobby the hobby to set
+	 */
+	public void setHobby(String hobby) {
+		this.hobby = hobby;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "InstructorDetail [id=" + id + ", youtubeChannel=" + youtubeChannel + ", hobby=" + hobby + "]";
+	}
+	
+	
+	
+}
